720d7070d8d8be38103bdbcd5254882bf3612bdd
{kjy} <gimjuyeong0620@gmail.com>
2023-08-24 14:28:19 +0900

    init: init project

---
+++ src/main/java/com/blow/server/api/BlowServerApplication.java
@@ -0,0 +1,13 @@
+package com.blow.server.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlowServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blow/server/api/BlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blow.server.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
