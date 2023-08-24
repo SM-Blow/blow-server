a55870cd270b0e98ee60ab953e63f871d635d3ab
{kjy} <gimjuyeong0620@gmail.com>
2023-08-24 21:17:12 +0900

    ErrorHandler 설정

---
+++ src/main/java/com/blow/server/api/common/ErrorHandler.java
@@ -0,0 +1,18 @@
+package com.blow.server.api.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleEntityNotFoundException(EntityNotFoundException exception){
+        ApiResponse response = ApiResponse.fail(exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
