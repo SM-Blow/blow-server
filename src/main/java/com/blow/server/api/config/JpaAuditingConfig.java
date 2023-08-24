8ad661b86609565eb8abcffb9d5ad712918574fe
{kjy} <gimjuyeong0620@gmail.com>
2023-08-24 21:14:10 +0900

    JpaAuditingConfig 설정

--- src/main/java/com/blow/server/api/config/JpaAuditingConfig.java
+++ src/main/java/com/blow/server/api/config/JpaAuditingConfig.java
@@ -1,4 +1,9 @@
 package com.blow.server.api.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
 public class JpaAuditingConfig {
 }
