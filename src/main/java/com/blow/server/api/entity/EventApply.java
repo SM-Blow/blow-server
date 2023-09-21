50e8e944d2fb541f0cdeaa62b2746975886b5e31
iamrae <iamrae@swallaby.com>
2023-09-21 14:38:50 +0900

    feat: Event Apply 엔티티 추가 #36

---
+++ src/main/java/com/blow/server/api/entity/EventApply.java
@@ -0,0 +1,45 @@
+package com.blow.server.api.entity;
+
+import com.blow.server.api.entity.superclass.TimeStamped;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Table(name = "\"EventApply\"")
+@Entity
+public class EventApply extends TimeStamped {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "event_apply_id")
+    private int id;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    private void setUser(User user){
+        if(Objects.nonNull(this.user)){
+            this.user.getEventApplyList().remove(this);
+        }
+        this.user = user;
+        user.getEventApplyList().add(this);
+    }
+
+    private void setEvent(Event event) {
+        if(Objects.nonNull(this.event)){
+            this.event.getEventApplyList().remove(this);
+        }
+        this.event = event;
+        event.getEventApplyList().add(this);
+    }
+}
