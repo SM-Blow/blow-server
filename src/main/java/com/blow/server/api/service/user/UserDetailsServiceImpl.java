2bafce9e37e8b3720e2ff81d890d655fa28cab08
iamrae <iamrae@swallaby.com>
2023-09-22 15:56:47 +0900

    fix: 인증 토큰 관련 예외 처리 #39

--- src/main/java/com/blow/server/api/service/user/UserDetailsServiceImpl.java
+++ src/main/java/com/blow/server/api/service/user/UserDetailsServiceImpl.java
@@ -1,11 +1,13 @@
 package com.blow.server.api.service.user;
 
 
+import com.blow.server.api.common.exception.TokenException;
 import com.blow.server.api.common.message.ExceptionMessage;
 import com.blow.server.api.entity.BlowUserDetails;
 import com.blow.server.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,8 +25,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String userId) {
+
         val user = userRepository.findById(Long.parseLong(userId))
-                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND_USER.getMessage()));
+                .orElseThrow(() -> new TokenException(ExceptionMessage.NOT_FOUND_USER_WITH_TOKEN.getMessage(), HttpStatus.NOT_FOUND));
 
         return BlowUserDetails.builder()
                 .id(user.getId())
