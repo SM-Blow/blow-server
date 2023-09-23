28ab9a674a9795be0dfe3277d066e24bce631364
gahee99 <u1879@sookmyung.ac.kr>
2023-09-23 10:15:36 +0900

    feat : swagger security 통과 막기 #51

--- src/main/java/com/blow/server/api/config/SecurityConfig.java
+++ src/main/java/com/blow/server/api/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers(     "/v3/api-docs", "/swagger-resources/**", "/swagger-ui/index.html", "/swagger-ui.html","/webjars/**", "/swagger/**").permitAll()
                 .and()
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtExceptionFilter, JwtTokenFilter.class)
