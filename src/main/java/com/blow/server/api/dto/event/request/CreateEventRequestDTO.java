608a6a11744e779e81619924a7943b2142d3ef79
gahee99 <u1879@sookmyung.ac.kr>
2023-09-23 09:01:42 +0900

    fix: Event 생성- 허용인원 관련 최소 인원 적용

--- src/main/java/com/blow/server/api/dto/event/request/CreateEventRequestDTO.java
+++ src/main/java/com/blow/server/api/dto/event/request/CreateEventRequestDTO.java
@@ -1,5 +1,6 @@
 package com.blow.server.api.dto.event.request;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -15,7 +16,8 @@ public record CreateEventRequestDTO (
         @NotBlank
         @NotNull
         String content,
-        @NotNull
+        @NotNull(message = "허용인원 수를 명시해 주세요.")
+        @Min(value = 1, message = "허용 가능한 인원은 1 이상이어야 합니다.")
         Long acceptCount,
         @NotNull
         LocalDateTime dueDate
