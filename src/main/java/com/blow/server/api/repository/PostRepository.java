9ddf2e2be381b996cd2f92aacec6c87514e894ad
김주영 <gimjuyeong0620@gmail.com>
2023-10-05 22:35:03 +0900

    fix : 전체 게시글 조회, 카테고리 별 조회 시 status 3 아닌것만 출력

--- src/main/java/com/blow/server/api/repository/PostRepository.java
+++ src/main/java/com/blow/server/api/repository/PostRepository.java
@@ -10,7 +10,11 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> getPostById(Long postId);
-    List<Post> findPostByCategoryOrderByCreatedAtDesc(String category);
+
+    @Query("SELECT p FROM Post p WHERE p.status != 3 ORDER BY p.createdAt DESC")
+    List<Post> findAll();
+    @Query("SELECT p FROM Post p WHERE LOWER(p.category) LIKE %:category% and p.status != 3 ORDER BY p.createdAt DESC")
+    List<Post> findPostByCategoryOrderByCreatedAtDesc( @Param("category") String category);
     List<Post> findAllByUserIdOrderByCreatedAtDesc(Long userId);
     @Query("SELECT p FROM Post p WHERE LOWER(p.title) LIKE %:postTitleKeyword% OR LOWER(p.content) LIKE %:postContentKeyword% ORDER BY p.createdAt DESC")
     List<Post> findAllByKeyword(
